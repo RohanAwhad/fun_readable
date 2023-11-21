efdf5c9345827f88229a340837345e05796430ff
Rohan Awhad <rohanavad007@gmail.com>
2023-11-21 16:27:21 -0700

    Refactor API code and add URLInput and TextOutput models

--- readable_service/api.py
+++ readable_service/api.py
@@ -2,29 +2,45 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from readability import Readable
+from pydantic import BaseModel
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware,
-    allow_origins=['*'],
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get('/healthcheck')
-def healthcheck():
-    return 'OK'
 
-@app.get('/convert')
-def convert(url: str):
+@app.get("/healthcheck")
+def healthcheck():
+    return "OK"
+
+
+class URLInput(BaseModel):
+    url: str
+
+
+class TextOutput(BaseModel):
+    title: str
+    article_content: str
+    html_content: str
+
+
+@app.post("/convert", response_model=TextOutput)
+def convert(url: URLInput):
     tmp = Readable(url)
     res = {
-        'title': tmp.title,
-        'article_content': str(tmp.article_content),
-        'html_content': str(tmp.html_content)
+        "title": tmp.title,
+        "article_content": str(tmp.article_content),
+        "html_content": str(tmp.html_content),
     }
     return res
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=50501)
+
+    uvicorn.run(app, host="0.0.0.0", port=50501)
