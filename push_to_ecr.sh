7238d7d363382eefc987089ae0b32c2ccea5bbab
Rohan Awhad <rohanavad007@gmail.com>
2024-01-25 14:57:08 -0700

    Update Dockerfile to build for Lambda

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -4,7 +4,7 @@ version=$1
 image_uri=${REPO_ARN}:v$version
 echo $image_uri
 
-docker build -t $image_uri . --platform linux/amd64
+docker build -t $image_uri -f Dockerfile.lambda . --platform linux/amd64
 docker push $image_uri
 aws lambda update-function-code \
   --function-name fun_readable \
